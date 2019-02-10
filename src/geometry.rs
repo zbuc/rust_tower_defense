26396ccdc1d0f53671050f671de8806b7b3b2b1e
Chris Czub <chris.czub@gmail.com>
2019-02-10 00:33:47 -0500

    Add better methods.

--- src/geometry.rs
+++ src/geometry.rs
@@ -3,11 +3,29 @@ use crate::serde_derive::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Point(pub u32, pub u32);
 
+impl Point {
+    pub fn x(&self) -> &u32 {
+        &self.0
+    }
+
+    pub fn y(&self) -> &u32 {
+        &self.1
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct BoundingBox(pub Point, pub Point);
 
 impl BoundingBox {
     pub fn area(&self) -> u32 {
-        ((self.1).0 - (self.0).0) * ((self.1).1 - (self.0).1)
+        (self.upper_right().x() - self.lower_left().x()) * (self.upper_right().y() - self.lower_left().y())
+    }
+
+    pub fn lower_left(&self) -> &Point {
+        &self.0
+    }
+
+    pub fn upper_right(&self) -> &Point {
+        &self.1
     }
 }
