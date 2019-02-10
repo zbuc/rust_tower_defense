a8db7eb74ce6f9b3aedaadb339a6f9a16622a04f
Chris Czub <chris.czub@gmail.com>
2019-02-09 23:37:35 -0500

    create new file correctly

--- src/game.rs
+++ src/game.rs
@@ -39,30 +39,35 @@ pub struct ActiveGame {
     pub started_time: u32,
 }
 
+pub fn recreate_default_map() -> Result<map::GameMap, Box<dyn Error>> {
+    println!("Recreating default map");
+
+    let mut file = File::create(DEFAULT_MAP)?;
+
+    let map_struct = map::GameMap {
+        dimensions: geometry::BoundingBox(geometry::Point(0, 0), geometry::Point(100, 100)),
+        name: "Default Map".to_string(),
+    };
+
+    let encoded: Vec<u8> = serialize(&map_struct).unwrap();
+    file.write(&encoded)?;
+
+    Ok(map_struct)
+}
+
 pub fn get_default_map() -> Result<map::GameMap, Box<dyn Error>> {
     // I will probably want to use some human-readable JSON config for top-level
     // map configurations.
-    let mut map_file = File::open(DEFAULT_MAP)?;
+    let mut map_file = match File::open(DEFAULT_MAP) {
+        Ok(f) => f,
+        Err(error) => return recreate_default_map(),
+    };
     let mut map_data = Vec::<u8>::new();
     map_file.read_to_end(&mut map_data)?;
 
     match deserialize(&map_data) {
         Ok(map_struct) => Ok(map_struct),
-        Err(error) => {
-            println!("Recreating default map");
-
-            let mut file = File::create(DEFAULT_MAP)?;
-
-            let map_struct = map::GameMap {
-                dimensions: geometry::BoundingBox(geometry::Point(0, 0), geometry::Point(100, 100)),
-                name: "Default Map".to_string(),
-            };
-
-            let encoded: Vec<u8> = serialize(&map_struct).unwrap();
-            file.write(&encoded)?;
-
-            Ok(map_struct)
-        }
+        Err(error) => recreate_default_map(),
     }
 }
 
