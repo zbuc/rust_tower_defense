296d6208f3bc1ef62c46ca75bd8158308b2c481a
Chris Czub <chris.czub@gmail.com>
2019-02-13 23:07:44 -0500

    Cacher impl supports multiple args

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,7 @@
 pub mod entities;
 pub mod map;
 
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::{self, File};
 use std::io::Read;
@@ -42,7 +43,7 @@ struct Cacher<T>
     where T: Fn(u32) -> u32
 {
     calculation: T,
-    value: Option<u32>,
+    values: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -51,20 +52,31 @@ impl<T> Cacher<T>
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            values: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+        match self.values.get(&arg) {
+            Some(v) => *v,
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.values.insert(arg, v);
                 v
             },
         }
     }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v1, 1);
+    assert_eq!(v2, 2);
 }
 
 pub fn recreate_default_map() -> Result<GameMap, Box<dyn Error>> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,7 @@
 pub mod game;
 pub mod geometry;
 
-extern crate bincode;
 extern crate serde;
+extern crate bincode;
 extern crate serde_derive;
 extern crate serde_json;
-
-use std::error::Error;
-use std::fs::{self, File};
-use std::io::Read;
-use std::io::Write;
-
-use crate::bincode::{deserialize, serialize};
-use game::entities::GameEntity;
-use game::map::{GameMap, DEFAULT_MAP, DEFAULT_MAP_DIMENSIONS, DEFAULT_MAP_NAME};
