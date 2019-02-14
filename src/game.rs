e970cede756d9410765faf14eb0ddd69f18dc5b1
Chris Czub <chris.czub@gmail.com>
2019-02-13 22:59:00 -0500

    copy paste Cacher implementation

--- src/game.rs
+++ src/game.rs
@@ -38,6 +38,35 @@ pub struct ActiveGame {
     pub started_time: u32,
 }
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
 pub fn recreate_default_map() -> Result<GameMap, Box<dyn Error>> {
     println!("Recreating default map");
 
