ed0994c5520378856cf4f68ad9c43013048e7d42
Chris Czub <chris.czub@gmail.com>
2019-02-16 21:56:59 -0500

    Select graphics adapter

--- src/graphics.rs
+++ src/graphics.rs
@@ -8,6 +8,7 @@
     allow(dead_code, unused_extern_crates, unused_imports)
 )]
 
+extern crate log;
 extern crate env_logger;
 #[cfg(feature = "dx12")]
 extern crate gfx_backend_dx12 as back;
@@ -23,7 +24,10 @@ extern crate glsl_to_spirv;
 extern crate image;
 extern crate winit;
 
+use hal::{queue, Adapter, Instance, QueueFamily};
 use winit::{dpi, ControlFlow, Event, EventsLoop, Window, WindowBuilder, WindowEvent};
+
+use log::Level;
 
 static WINDOW_NAME: &str = "Rust Tower Defense 0.1.0";
 
@@ -33,8 +37,17 @@ static WINDOW_NAME: &str = "Rust Tower Defense 0.1.0";
 /// Eventually we'll have a command system responsible
 /// for telling the window to close.
 pub fn run() {
+    // if building in debug mode, vulkan backend initializes standard validation layers
+    // all we need to do is enable logging
+    // run the program like so to print all logs of level 'warn' and above:
+    // bash: RUST_LOG=warn && cargo run --bin 02_validation_layers --features vulkan
+    // powershell: $env:RUST_LOG="warn"; cargo run --bin 02_validation_layers --features vulkan
+    // see: https://docs.rs/env_logger/0.5.13/env_logger/
+    env_logger::init();
+    info!("Starting the application!");
     let mut application = RustTowerDefenseApplication::init();
     application.run();
+    info!("Cleaning up the application!");
     application.clean_up();
 }
 
@@ -46,12 +59,28 @@ struct WindowState {
 
 /// The state object for the graphics backend.
 struct HalState {
+    _adapter: Adapter<back::Backend>,
     _instance: back::Instance,
 }
 
 impl HalState {
     /// No clean-up
     fn clean_up(self) {}
+}
+
+/// Hold a graphics family that has been selected for the graphics
+/// display queue. This allows us to describe the queue features supported
+/// by the graphics display device.
+#[derive(Default)]
+struct QueueFamilyIds {
+    graphics_family: Option<queue::QueueFamilyId>,
+}
+
+impl QueueFamilyIds {
+    /// Return whether the queue family is supported by the graphics device.
+    fn is_complete(&self) -> bool {
+        self.graphics_family.is_some()
+    }
 }
 
 /// The application contains the window and graphics
@@ -100,13 +129,53 @@ impl RustTowerDefenseApplication {
     /// Creates a new instance of the graphics device's state
     fn init_hal() -> HalState {
         let instance = RustTowerDefenseApplication::create_device_instance();
+        let adapter = RustTowerDefenseApplication::pick_adapter(&instance);
+
         HalState {
+            _adapter: adapter,
             _instance: instance,
         }
     }
 
+    /// Finds the queue families supported by an Adapter, and returns
+    /// the results in a QueueFamilyIds struct.
+    fn find_queue_families(adapter: &Adapter<back::Backend>) -> QueueFamilyIds {
+        let mut queue_family_ids = QueueFamilyIds::default();
+
+        for queue_family in &adapter.queue_families {
+            if queue_family.max_queues() > 0 && queue_family.supports_graphics() {
+                queue_family_ids.graphics_family = Some(queue_family.id());
+            }
+
+            if queue_family_ids.is_complete() {
+                break;
+            }
+        }
+
+        queue_family_ids
+    }
+
+    /// Returns true if the adapter supports the necessary queue families
+    /// for the application.
+    fn is_adapter_suitable(adapter: &Adapter<back::Backend>) -> bool {
+        RustTowerDefenseApplication::find_queue_families(adapter).is_complete()
+    }
+
+    /// Iterate every display adapter available to the display backend
+    /// and select the first suitable adapter.
+    fn pick_adapter(instance: &back::Instance) -> Adapter<back::Backend> {
+        let adapters = instance.enumerate_adapters();
+        for adapter in adapters {
+            if RustTowerDefenseApplication::is_adapter_suitable(&adapter) {
+                return adapter;
+            }
+        }
+        panic!("No suitable adapter");
+    }
+
     /// Runs window state's event loop until a CloseRequested event is received
     fn main_loop(&mut self) {
+        info!("Starting event loop...");
         self.window_state
             .events_loop
             .run_forever(|event| match event {
@@ -120,6 +189,7 @@ impl RustTowerDefenseApplication {
 
     /// Runs the application's main loop function.
     fn run(&mut self) {
+        info!("Running application...");
         self.main_loop();
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,5 @@ extern crate bincode;
 extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
+
+#[macro_use] extern crate log;
